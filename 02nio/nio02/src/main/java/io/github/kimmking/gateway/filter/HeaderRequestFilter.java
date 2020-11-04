5bad0a68dccf1a9bf31dacf2d571e7c52a4161fe
nova <nova_woo@foxmail.com>
2020-11-04 19:38:00 +0800

    提交nio作业

---
+++ 02nio/nio02/src/main/java/io/github/kimmking/gateway/filter/HeaderRequestFilter.java
@@ -0,0 +1,12 @@
+package io.github.kimmking.gateway.filter;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.FullHttpRequest;
+
+public class HeaderRequestFilter implements HttpRequestFilter {
+    @Override
+    public void filter(FullHttpRequest fullRequest, ChannelHandlerContext ctx) {
+        // 在headers中增加一对属性 "nio:nova"
+        fullRequest.headers().add("nio","nova");
+    }
+}
