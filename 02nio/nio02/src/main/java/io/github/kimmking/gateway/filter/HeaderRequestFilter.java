c229a764d8f0791a981feefc9d366e5ad7bae9b3
nova <nova_woo@foxmail.com>
2020-11-08 10:12:07 +0800

    增加注释

--- 02nio/nio02/src/main/java/io/github/kimmking/gateway/filter/HeaderRequestFilter.java
+++ 02nio/nio02/src/main/java/io/github/kimmking/gateway/filter/HeaderRequestFilter.java
@@ -6,7 +6,7 @@ import io.netty.handler.codec.http.FullHttpRequest;
 public class HeaderRequestFilter implements HttpRequestFilter {
     @Override
     public void filter(FullHttpRequest fullRequest, ChannelHandlerContext ctx) {
-        // 在headers中增加一对属性 "nio:nova"
+       // 在headers中增加一对属性 "nio:nova"
         fullRequest.headers().add("nio","nova");
     }
 }
