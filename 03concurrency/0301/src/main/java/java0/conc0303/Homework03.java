33a46cc83b30844ca914c0a035257c0834c1d133
nova <nova_woo@foxmail.com>
2020-11-11 23:58:36 +0800

    增加第四课作业

--- 03concurrency/0301/src/main/java/java0/conc0303/Homework03.java
+++ 03concurrency/0301/src/main/java/java0/conc0303/Homework03.java
@@ -1,5 +1,9 @@
 package java0.conc0303;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
 /**
  * 本周作业：（必做）思考有多少种方式，在main函数启动一个新线程或线程池，
  * 异步运行一个方法，拿到这个方法的返回值后，退出主线程？
@@ -9,17 +13,25 @@ package java0.conc0303;
  */
 public class Homework03 {
     
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+
         long start=System.currentTimeMillis();
+
         // 在这里创建一个线程或线程池，
         // 异步执行 下面方法
-        
-        int result = sum(); //这是得到的返回值
-        
+
+        FutureTask<Integer> task = new FutureTask<Integer>(new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                return sum();
+            }
+        });
+
+        new Thread(task).start();
+        int result = task.get();
+
         // 确保  拿到result 并输出
         System.out.println("异步计算结果为："+result);
-         
         System.out.println("使用时间："+ (System.currentTimeMillis()-start) + " ms");
         
         // 然后退出main线程
